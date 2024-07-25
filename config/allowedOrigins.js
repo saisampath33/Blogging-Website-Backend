caedffffe2a35316a28a1fa06dca238e06e86c94
Korada Sai Sampath Kumar <118899372+saisampath33@users.noreply.github.com>
2024-07-25 16:12:03 +0530

    Add files via upload
    This is my backend repo

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:4200',
+    'http://localhost:5174',
+    'http://localhost:5175'
+
+];
+
+module.exports = allowedOrigins;
